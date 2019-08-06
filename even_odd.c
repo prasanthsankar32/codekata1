567ffbd28ec65f6a8ab8f829511e8dd07cdc9da4
prasanth <52153270+prasanthsankar32@users.noreply.github.com>
2019-08-06 14:23:37 +0530

    Add files via upload

---
+++ even_odd.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    if(n<0)
+    {
+        printf("invalid");
+    }
+    else if(n%2==0)
+    {
+        printf("even");
+        
+    }
+    else
+    {
+        printf("odd");
+    }
+}

5d714e8bc9b82d10b918c8c8f510f8c26f7d42b2
prasanth <52153270+prasanthsankar32@users.noreply.github.com>
2019-08-07 01:00:16 +0530

    Add files via upload

---
+++ string_splchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char str[100];
+    int i=0,count=0;
+    gets(str);
+    while (str[i] != '\0')
+    {
+        if (str[i] >=33 && str[i]<=47 || str[i] >=58 && str[i]<=64 || str[i] >=123 && str[i]<=126)
+        {
+            count++;
+        }
+        i++;
+    }
+    printf("%d",count);
+}

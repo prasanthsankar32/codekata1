77b447a34bc0c6936a977e0bb9144d3c26cad09c
prasanth <52153270+prasanthsankar32@users.noreply.github.com>
2019-08-07 01:07:18 +0530

    Update string_splchar.c

--- string_splchar.c
+++ string_splchar.c
@@ -7,7 +7,7 @@ int main()
     gets(str);
     while (str[i] != '\0')
     {
-        if (str[i] >=33 && str[i]<=47 || str[i] >=58 && str[i]<=64 || str[i] >=123 && str[i]<=126 || str[i]==' ')
+        if (str[i] >=33 && str[i]<=47 || str[i] >=58 && str[i]<=64 || str[i] >=123 && str[i]<=126 || str[i]>=91 && str[i] <=96)
         {
             count++;
         }

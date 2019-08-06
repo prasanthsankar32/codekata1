197d795b2f85a400cc9ae7907d4446278acd2185
prasanth <52153270+prasanthsankar32@users.noreply.github.com>
2019-08-07 00:36:40 +0530

    Add files via upload

---
+++ string_lines.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char str[100];
+    int i=0,count=0;
+    gets(str);
+    while (str[i] != '\0')
+    {
+        if (str[i] == '.')
+        {
+            count++;
+        }
+        i++;
+    }
+    printf("%d",count+1);
+}

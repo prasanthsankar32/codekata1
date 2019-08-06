7e415cd3c7e7307ea5eb1d46e6d30132e54958d3
prasanth <52153270+prasanthsankar32@users.noreply.github.com>
2019-08-06 16:12:36 +0530

    Add files via upload

---
+++ sum_k_integer.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+  int n,k,i,sum;
+  scanf("%d",&n);
+  scanf("%d",&k);
+  for(i=1;i<=n;i++)
+	{
+	scanf("%d",&i);
+	}
+	  for(i=0;i<=k;i++)
+    {
+        sum += i;
+    }
+    printf("%d",sum);
+}

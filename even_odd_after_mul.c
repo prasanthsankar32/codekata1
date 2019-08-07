ad9150f365b9a8385135e57b137a0a5f38df79a0
prasanth <52153270+prasanthsankar32@users.noreply.github.com>
2019-08-07 10:38:15 +0530

    Add files via upload

---
+++ even_odd_after_mul.c
@@ -0,0 +1,17 @@
+    
+#include <stdio.h>
+#include<string.h>
+int main()
+{
+    int n1,n2,mul;
+    scanf("%d%d",&n1,&n2);
+    mul=n1*n2;
+    if(mul%2==0)
+    {
+        printf("even");
+    }
+    else
+    {
+        printf("odd");
+    }
+}
